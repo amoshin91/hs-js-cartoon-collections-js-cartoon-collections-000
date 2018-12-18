d4e8df3f1718c7da2e9a56eab0ca0a4550c636fe
Yongwoo Shin <amoshinihsoma@gmail.com>
2018-12-18 21:06:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 function dwarfRollCall(dwarves) {
+  var names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  let dwarfCall = []
+  for (let i = 0; i < dwarves.length; i++){
+    dwarfCall.push([i+1]+ '. '+ dwarves[i])
+  }
+  
+  return dwarfCall.join(' ') + " "
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  let planet = [];
+  let i = 0;
+  
+  while (i < planeteerCalls.length){
+    planet.push(planeteerCalls[i].toUpperCase() + '!');
+    i++;
+  }
+
+  return planet
 }
 
 function longPlaneteerCalls(words) {
+  var word = false
+  for (let i = 0; i < words.length; i++){
+    if (words[i].length > 4){
+    }
+    
+  return word
+  }
 }
 
 function findTheCheese (foods) {
